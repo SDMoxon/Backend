75e6c4684537ef1646dc6b75ab754cdf39a1a184
Steven <steven_moxon123@hotmail.co.uk>
2017-07-18 16:57:46 +0100

    [.addPatients] refactored to use req.body

--- functions/index.js
+++ functions/index.js
@@ -4,9 +4,9 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 exports.addPatient = functions.https.onRequest((req,res) => {
-    const original = req.query.text;
+    const original = req.body;
 
-    admin.database().ref('/patients').set({original: original})
+    admin.database().ref('/patients').push(original)
     .then(snapshot => {
         res.redirect(303, snapshot.ref);
     });
