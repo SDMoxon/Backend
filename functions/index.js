14f6c1e71a58ad4ebf870a8b1779de1320dc11ab
alanionita <a.g.ionita@gmail.com>
2017-07-19 11:29:12 +0100

    [addPatients] refactored the function so that it pushes imputs into the DB

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,16 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const bodyParser = require('body-parser');
 
 admin.initializeApp(functions.config().firebase);
 
 // POST /patients/$newPatient
 exports.addPatient = functions.https.onRequest((req,res) => {
+    bodyParser.json();
     const patient = req.body;
-
+    
     admin.database().ref('/patients').push(patient)
-    .then(snapshot => {
-        res.redirect(303, snapshot.val());
-    });
+    res.status(200).send('Patient added!')
 });
 
 // GET /patients/
