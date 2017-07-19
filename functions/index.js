c45ac40641461ae4da9ccc7eedb1ff967e2cecdf
Steven <steven_moxon123@hotmail.co.uk>
2017-07-19 15:29:29 +0100

    [getPatientByWard] implement functiion

--- functions/index.js
+++ functions/index.js
@@ -4,31 +4,31 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 // POST /patients/$newPatient
-exports.addPatient = functions.https.onRequest((req,res) => {
+exports.addPatient = functions.https.onRequest((req, res) => {
     const patient = req.body;
 
     admin.database().ref('/patients').push(patient)
-    .then(snapshot => {
-        res.redirect(303, snapshot.val());
-    });
+        .then(snapshot => {
+            res.redirect(303, snapshot.val());
+        });
 });
 
 // GET /patients/
 
-exports.getPatients = functions.https.onRequest((req,res) => {
+exports.getPatients = functions.https.onRequest((req, res) => {
     admin.database().ref('/patients').once('value', function (snapshot) {
         res.status(200).send(snapshot.val());
     });
 });
 
 // get /wards/
-exports.getWards = functions.https.onRequest((req,res) => {
+exports.getWards = functions.https.onRequest((req, res) => {
     admin.database().ref('/wards').once('value', function (snapshot) {
         res.status(200).send(snapshot.val());
     });
 });
 
-exports.getPatientById = functions.https.onRequest((req,res) => {
+exports.getPatientById = functions.https.onRequest((req, res) => {
     const id = req.query.id;
     console.log(id);
     admin.database().ref(`/patients/${id}`).once('value', function (snapshot) {
@@ -36,12 +36,18 @@ exports.getPatientById = functions.https.onRequest((req,res) => {
     });
 });
 
-exports.getPatientsByWard = functions.https.onRequest((req,res) => {
+exports.getPatientsByWard = functions.https.onRequest((req, res) => {
+    const wardQuery = req.query.ward;
+    let filterKeys;
     admin.database().ref('/patients').once('value', function (snapshot) {
-        res.status(200).send(
-            Object.keys(snapshot.val()).filter((key) => {
-                return snapshot.val()[key].wardName === 'Acute Neurology Unit';
-            })
-        );
+       filterKeys = Object.keys(snapshot.val()).reduce((acc, key) => {
+                if (snapshot.val()[key].wardName === wardQuery) { 
+                    acc[key] = snapshot.val()[key];
+                    return acc;
+                }
+                    return acc;
+            }, {});
+        res.status(200).send(filterKeys);
+            
     });
 });
