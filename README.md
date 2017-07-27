2b1a0de1ce26674ab5594f104b23aa44b0d9eaa6
Alan Ionita <alanionita@users.noreply.github.com>
2017-07-27 16:01:27 +0100

    Cloud functions branch - Merge! (#2)
    * initial commit for cloud functions
    
    * added queries to the /patients route
    
    * [.addPatients] refactored to use req.body
    
    * [getPatients] added the cloud function
    
    * add get patients by ward with static parameters
    
    * [addPatients] refactored the function so that it pushes imputs into the DB
    
    * add getPatientById function
    
    * [getPatientByWard] implement functiion
    
    * integrated incoming changes from master
    
    * [putVitals] function will update vitals on databse
    
    * update cloud functions to work with new data model
    
    * [postCareLog]  addded one new functions and tested via BDD
    
    * [patientByName] add function and test by BDD
    
    * upate readme with endpoints
    
    * [postCareLog][addPatient] added an http response to our post routes
    
    * [seed file] randomly generate patient details and push them to the database
    
    * [seed file] randomise more fields
    
    * [putPatientDetails] added an http response to the function
    
    * [putMedication] added an http response to the function
    
    * [putVitals] added an http response to the function
    
    * [seed file] randomise patient details address
    
    * add randomised test results to patients
    
    * refactor object to be accepted by firebase database no 0s or nulls
    
    * remove wardName from patient details
    
    * rebase
    
    * [addPatient] respond with request object
    
    * [putVitals] respond with request object
    
    * [putMedication] respond with request object
    
    * [postCareLog] respond with request object
    
    * [functions] added CORS to enable front-end calls
    
    * [functions] required another rebase: just noticed all of the changes
    
    * [seed-file] updated to seed to include a name key in various locations
    
    * [cloud_functions] updated putPatientDetails to use Firebase update(), updated postCareLog, added putCareLog function
    
    * preparing to add wards to patients
    
    * [seed] added random wardNames to the seed file
    
    * added better route comments

--- README.md
+++ README.md
@@ -7,7 +7,17 @@ An app designed for doctors with the aim of making their lives easier. Instead o
 * Google Cloud functions
 
 ## How to use it
-* [endpoints]
+* Function URL (addPatient): https://us-central1-live-northcoders-nhs-app.cloudfunctions.net/addPatient
+* nction URL (getPatients): https://us-central1-live-northcoders-nhs-app.cloudfunctions.net/getPatients
+* Function URL (getWards): https://us-central1-live-northcoders-nhs-app.cloudfunctions.net/getWards
+* Function URL (getPatientById)      :https://us-central1-live-northcoders-nhs-app.cloudfunctions.net/getPatientById
+* Function URL (getPatientsByWard):https://us-central1-live-northcoders-nhs-app.cloudfunctions.net/getPatientsByWard
+* Function URL (putVitals): https://us-central1-live-northcoders-nhs-app.cloudfunctions.net/putVitals
+* Function URL (putMedication):  https://us-central1-live-northcoders-nhs-app.cloudfunctions.net/putMedication
+* Function URL (putPersonalDetails): https://us-central1-live-northcoders-nhs-app.cloudfunctions.net/putPersonalDetails
+* Function URL (postCareLog): https://us-central1-live-northcoders-nhs-app.cloudfunctions.net/postCareLog
+* Function URL (putCareLog): https://us-central1-live-northcoders-nhs-app.cloudfunctions.net/putCareLog
+* Function URL (patientByName): https://us-central1-live-northcoders-nhs-app.cloudfunctions.net/patientByName
 
 ## Sprint History
 
---
+++ functions/index.js
@@ -0,0 +1,159 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const bodyParser = require('body-parser');
+const cors = require('cors')({ origin: true });
+
+admin.initializeApp(functions.config().firebase);
+
+// POST /patients
+exports.addPatient = functions.https.onRequest((req, res) => {
+    bodyParser.json();
+    const patient = req.body;
+    admin.database().ref('/patients').push(patient);
+    
+    cors(req, res, () => {
+        res.status(200).send(patient);
+    });
+});
+
+// GET /patients
+exports.getPatients = functions.https.onRequest((req, res) => {
+    admin.database().ref('/patients').once('value', function (snapshot) {
+        cors(req, res, () => {
+            res.status(200).send(snapshot.val());
+        });
+    });
+});
+
+// GET /wards
+exports.getWards = functions.https.onRequest((req, res) => {
+    admin.database().ref('/wards').once('value', (snapshot) => {
+        cors(req, res, () => {
+            res.status(200).send(snapshot.val());
+        });
+    });
+});
+
+// GET /patients/:id
+exports.getPatientById = functions.https.onRequest((req, res) => {
+    const id = req.query.id;
+
+    admin.database().ref(`/patients/${id}`).once('value', function (snapshot) {
+        cors(req, res, () => {
+            res.status(200).send(snapshot.val());
+        });
+    });
+});
+
+// GET /patients/:ward
+exports.getPatientsByWard = functions.https.onRequest((req, res) => {
+    const wardQuery = req.query.ward;
+    let filterKeys;
+
+    admin.database().ref('/patients').once('value', (snapshot) => {
+        filterKeys = Object.keys(snapshot.val()).reduce((acc, key) => {
+            if (snapshot.val()[key].wardName === wardQuery) {
+                acc[key] = {
+                    'name': snapshot.val()[key].personalDetails.firstNames[0] + ' ' + snapshot.val()[key].personalDetails.surname,
+                    'NHS number': snapshot.val()[key].personalDetails.NHSnumber,
+                    'condition': snapshot.val()[key].currentMedicalState.currentCondition
+                };
+                return acc;
+            }
+            return acc;
+        }, {});
+        cors(req, res, () => {
+            res.status(200).send(filterKeys);
+        });
+    });
+});
+
+// PUT patients/:id/vitals
+exports.putVitals = functions.https.onRequest((req, res) => {
+    const patientId = req.query.id;
+    const dataObject = req.body;
+    const timestamp = new Date();
+
+    admin.database().ref(`/patients/${patientId}/vitals`).child(`${timestamp}`).set(dataObject);
+    cors(req, res, () => {
+        res.status(200).send(dataObject);
+    });
+});
+
+// PUT patients/:id/medication
+exports.putMedication = functions.https.onRequest((req, res) => {
+    const patientId = req.query.id;
+    const medicationId = req.query.medication;
+    const dataObject = req.body;
+
+    admin.database().ref(`/patients/${patientId}/medication`).child(`${medicationId}`).set(dataObject);
+    cors(req, res, () => {
+        res.status(200).send(dataObject);
+    });
+});
+
+// PUT patients/:id/personalDetails
+exports.putPersonalDetails = functions.https.onRequest((req, res) => {
+    const patientId = req.query.id;
+    const dataObject = req.body;
+
+    admin.database().ref(`/patients/${patientId}`).child('personalDetails').update(dataObject)
+        .then(
+            cors(req, res, () => {
+                res.status(200).send(dataObject);
+            })
+        );
+});
+
+// POST patients/:id/careLog
+exports.postCareLog = functions.https.onRequest((req, res) => {
+    bodyParser.json();
+    const patientId = req.query.id;
+    const newLog = req.body;
+    let timestamp = new Date();
+    newLog.createdAt = timestamp.toString();
+    newLog.done = false;
+
+    admin.database().ref(`/patients/${patientId}/careLog`).push(newLog);
+    cors(req, res, () => {
+        res.status(200).send(newLog);
+    });
+});
+
+// PUT patients/:id/careLog/:careLogId
+exports.putCareLog = functions.https.onRequest((req, res) => {
+    bodyParser.json();
+    const patientId = req.query.id;
+    const careLogId = req.query.careLogId;
+    const newLog = req.body;
+    let timestamp = new Date();
+    if (newLog.done === 'true') {
+        newLog.completedAt = timestamp.toString();
+    }
+
+    admin.database().ref(`/patients/${patientId}/careLog/${careLogId}`).update(newLog);
+    cors(req, res, () => {
+        res.status(200).send(newLog);
+    });
+});
+
+
+// GET patients/:id
+exports.patientByName = functions.https.onRequest((req, res) => {
+    const patientName = req.query.name;
+    const regex = new RegExp(patientName, 'gi');
+    let filterKeys; 
+
+    admin.database().ref('/patients').once('value', (snapshot) => {
+        filterKeys = Object.keys(snapshot.val()).reduce((acc, key) => {
+            if (regex.test(snapshot.val()[key].personalDetails.firstNames.join()) || regex.test(snapshot.val()[key].personalDetails.surname)) {
+                acc[key] = snapshot.val()[key];
+                return acc;
+            }
+            return acc;
+        }, {});
+        cors(req, res, () => {
+            res.status(200).send(filterKeys);
+        });
+    });
+});
